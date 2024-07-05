chore: Update learning rate in config.yaml and fix dice coefficient calculation in train.py

model/loss.py
@@ -18,14 +18,24 @@ def dice_bce_loss(logits, targets, smooth=1e-7, **kwargs):
     return bce + dice
 
 
-# mse loss
 @jax.jit
-def mse_loss(pred, target):
-    # softmax activation
-    # pred = jax.nn.softmax(pred, axis=-1)
-    # mse loss
-    loss = jnp.mean(jnp.square(pred - target))
-    return loss
+def dice_coef(pred, target, smooth=1.e-9):
+    pred = pred.flatten()
+    target = target.flatten()
+    transposed_pred = jnp.transpose(pred)
+    intersection = jnp.dot(target, transposed_pred)
+    union = jnp.dot(target, jnp.transpose(target)) + jnp.dot(pred, jnp.transpose(pred))
+    return 1 - (2. * intersection + smooth) / (union + smooth)
+
+
+@jax.jit
+def batch_dice_coef(pred, target):
+    dice = 0
+    """Dice coeff for batches"""
+    for i, p in enumerate(pred):
+        dice += dice_coef(p, target)
+
+    return dice / (i + 1)
 
 
 # focal loss
@@ -62,3 +72,12 @@ if __name__ == "__main__":
     pred = jnp.array([0.1, 0.2, 0.3, 0.4, 0.5])
     target = jnp.array([0, 1, 0, 1, 0])
     print(focal_loss(pred, target))
+
+    # dice_coef_loss(
+    #     K.theano.shared(np.array([[0,0,0],[0,0,0]])),
+    #     K.theano.shared(np.array([[0,0,0],[0,0,0]]))
+    # ).eval() # array([ 0.,  0.])
+
+    pred = jnp.array([[0, 0, 0], [1, 1, 1]])
+    target = jnp.array([[0, 0, 0]])
+    print(batch_dice_coef(pred, target))

model/unetv3_light.py
@@ -6,7 +6,6 @@ from flax import linen as nn
 class ConvBlock(nn.Module):
     features: int = 64
     n: int = 2
-    use_softmax: bool = False
     training: bool = True
 
     @nn.compact
@@ -20,10 +19,72 @@ class ConvBlock(nn.Module):
                 kernel_init=nn.initializers.kaiming_normal()
             )(x)
             x = nn.BatchNorm(use_running_average=not self.training)(x)
-            if self.use_softmax:
-                x = nn.softmax(x)
-            else:
-                x = nn.relu(x)
+            x = nn.relu(x)
+        return x
+
+
+class ConvUpsampleSoftmax(nn.Module):
+    features: int = 64
+    upsample: int = 2
+
+    @nn.compact
+    def __call__(self, x):
+        x = nn.Conv(self.features,
+                kernel_size=(3, 3),
+                strides=(1, 1),
+                padding='SAME',
+                kernel_init=nn.initializers.kaiming_normal()
+            )(x)
+        if self.upsample > 1:
+            x = jax.image.resize(x,
+                shape=(x.shape[0], x.shape[1] * self.upsample, x.shape[2] * self.upsample, x.shape[3]),
+                method='bilinear'
+            )
+        x = nn.softmax(x)
+        return x
+
+
+# spatial attention
+class SpatialAttention(nn.Module):
+    features: int = 64
+    training: bool = True
+
+    @nn.compact
+    def __call__(self, x):
+        avg_pool = nn.avg_pool(x, window_shape=(x.shape[1], x.shape[2]), strides=(1, 1))
+        max_pool = nn.max_pool(x, window_shape=(x.shape[1], x.shape[2]), strides=(1, 1))
+        x = jnp.concatenate([avg_pool, max_pool], axis=-1)
+        x = nn.Conv(self.features,
+            kernel_size=(1, 1),
+            strides=(1, 1),
+            kernel_init=nn.initializers.kaiming_normal()
+        )(x)
+        x = nn.sigmoid(x)
+        return x
+
+
+# STN module
+class STN(nn.Module):
+
+    @nn.compact
+    def __call__(self, x):
+        xs = nn.Conv(8, kernel_size=7, strides=1, padding='SAME')(x)
+        xs = nn.max_pool(xs, window_shape=(2, 2), strides=(2, 2))
+        xs = nn.relu(xs)
+        xs = nn.Conv(10, kernel_size=5, strides=1, padding='SAME')(xs)
+        xs = nn.max_pool(xs, window_shape=(2, 2), strides=(2, 2))
+        xs = nn.relu(xs)
+        xs = xs.reshape((-1, 90))
+
+        theta = nn.Sequential([
+            nn.Dense(32),
+            nn.relu,
+            nn.Dense(6),
+        ])(xs)
+        theta = theta.reshape((-1, 2, 3))
+
+        grid = jnp.linalg.inv(jnp.diag(jnp.ones((x.shape[0], 2)) + 1e-5)) @ theta
+        x = jnp.contrib.scipy.ndimage.map_coordinates(x, grid, order=1, mode='reflect')
         return x
 
 
@@ -58,7 +119,7 @@ class Decoder(nn.Module):
 
     @nn.compact
     def __call__(self, e1, e2, e3, e4, e5):
-        up_chans = self.features * len([e1, e2, e3, e4, e5])
+        up_chans = self.features * 4
 
         e3_d4 = nn.max_pool(e3, window_shape=(2, 2), strides=(2, 2))
         e3_d4 = ConvBlock(features=self.features, n=1, training=self.training)(e3_d4)
@@ -92,7 +153,7 @@ class Decoder(nn.Module):
         d4_d1 = jax.image.resize(d4, shape=(d4.shape[0], d4.shape[1] * 8, d4.shape[2] * 8, d4.shape[3]), method='bilinear')
         d4_d1 = ConvBlock(features=self.features, n=1, training=self.training)(d4_d1)
         d1 = jnp.concatenate([e1_d1, d2_d1, d3_d1, d4_d1], axis=-1)
-        d1 = ConvBlock(features=up_chans, n=1, training=self.training, use_softmax=True)(d1)
+        d1 = ConvBlock(features=up_chans, n=1, training=self.training)(d1)
 
         # branch for charmap
         char = ConvBlock(features=self.features, n=1, training=self.training)(d1)
@@ -101,75 +162,27 @@ class Decoder(nn.Module):
             kernel_size=(1, 1),
             strides=1,
             kernel_init=nn.initializers.kaiming_normal(),
-            use_bias=True,
         )(char)
 
-        # branch for ordmap
-        ord_ = ConvBlock(features=self.features * 4, n=1, training=self.training)(d1)
-        ord_ = nn.Conv(
-            features=self.ord_nums,
+        hmap = ConvBlock(features=self.features, n=1, training=self.training)(d1)
+        hmap = nn.Conv(
+            features=1,
             kernel_size=(1, 1),
             strides=1,
             kernel_init=nn.initializers.kaiming_normal(),
-            use_bias=True,
-        )(ord_)
+        )(hmap)
 
-        return char, ord_
+        # supervision
+        d1 = ConvUpsampleSoftmax(features=self.ord_nums, upsample=0)(d1)
 
+        if self.training:
+            d2 = ConvUpsampleSoftmax(features=self.ord_nums, upsample=2)(d2)
+            d3 = ConvUpsampleSoftmax(features=self.ord_nums, upsample=4)(d3)
+            d4 = ConvUpsampleSoftmax(features=self.ord_nums, upsample=8)(d4)
+            d5 = ConvUpsampleSoftmax(features=self.ord_nums, upsample=16)(e5)
+            return hmap, char, (d1, d2, d3, d4, d5)
 
-class UpSample(nn.Module):
-    up_repeat: int = 3
-    n_channels: int = 128
-
-    @nn.compact
-    def __call__(self, x, train=True):
-        for _ in range(self.up_repeat):
-            x = nn.Conv(
-                features=self.n_channels,
-                kernel_size=(3, 3),
-                strides=(1, 1),
-                padding="same",
-                kernel_init=nn.initializers.kaiming_normal(),
-                use_bias=False)(x)
-            x = nn.BatchNorm(use_running_average=not train)(x)
-            x = nn.PReLU()(x)
-            x = jax.image.resize(x,
-                                 shape=(x.shape[0], x.shape[1] * 2,
-                                        x.shape[2] * 2, x.shape[3]),
-                                 method="nearest")
-        return x
-
-
-# spatial attention
-class SpatialAttention(nn.Module):
-    features: int = 64
-
-    @nn.compact
-    def __call__(self, x):
-        avg_pool = jnp.mean(x, axis=-1, keepdims=True)
-        max_pool = jnp.max(x, axis=-1, keepdims=True)
-        pool = jnp.concatenate([avg_pool, max_pool], axis=-1)
-        pool = nn.Conv(self.features, kernel_size=(1, 1))(pool)
-        pool = nn.relu(pool)
-        pool = nn.Conv(1, kernel_size=(1, 1))(pool)
-        pool = nn.sigmoid(pool)
-        return pool
-
-
-# channel attention
-class ChannelAttention(nn.Module):
-    features: int = 64
-
-    @nn.compact
-    def __call__(self, x):
-        avg_pool = jnp.mean(x, axis=(1, 2), keepdims=True)
-        max_pool = jnp.max(x, axis=(1, 2), keepdims=True)
-        pool = jnp.concatenate([avg_pool, max_pool], axis=-1)
-        pool = nn.Conv(128, kernel_size=(1, 1))(pool)
-        pool = nn.relu(pool)
-        pool = nn.Conv(x.shape[-1], kernel_size=(1, 1))(pool)
-        pool = nn.sigmoid(pool)
-        return x * pool
+        return hmap, char, d1
 
 
 class UNetV3(nn.Module):
@@ -197,7 +210,7 @@ if __name__ == '__main__':
 
     key = jax.random.PRNGKey(0)
     model = UNetV3(16, training=True)
-    x = jnp.ones((1, 320, 320, 3))
+    x = jnp.ones((1, 256, 256, 3))
     params = model.init(key, x)
     out, batch_stats = model.apply(
         params, x,

train.py
@@ -14,16 +14,20 @@ from model.unetv3_light import UNetV3 as UNet
 
 @jax.jit
 def combine_loss(pred, target, n=16):
-    target_char = target[:, :, :, :1]
-    target_ord = target[:, :, :, 1:1+n]
+    # target_hmap = target[:, :, :, 0:1]
+    target_char = target[:, :, :, 1:2]
+    target_ord = target[:, :, :, 2:2+n]
 
-    pred_char, pred_ord = pred
+    pred_hmap, pred_char, pred_ord = pred
+    # pred_hmap = focal_loss(pred_hmap, target_hmap)
     loss_char = focal_loss(pred_char, target_char)
-    loss_ord = dice_bce_loss(pred_ord, target_ord)
-    loss = loss_char + loss_ord * 5
+    loss_ord = batch_dice_coef(pred_ord, target_ord)
+    # loss_ord = dice_coef(pred_ord, target_ord)
+    loss = loss_char + loss_ord * 10.0
 
     return loss, {
         'loss': loss,
+        # 'loss_hmap': pred_hmap,
         'loss_char': loss_char,
         'loss_ord': loss_ord,
     }
@@ -78,10 +82,13 @@ def predict(state: TrainState, batch):
 
 
 if __name__ == "__main__":
+    # cpu mode
+    jax.config.update("jax_platform_name", "cpu")
+
     key = jax.random.PRNGKey(0)
     x = jnp.zeros((1, cfg["img_size"], cfg["img_size"], 3))
 
-    model = UNet(cfg["features"], training=True)
+    model = UNet(cfg["features"], cfg["features"], training=True)
     var = model.init(key, x)
     params = var['params']
     batch_stats = var['batch_stats']

utils/pre_chars.py
@@ -1,5 +1,7 @@
 import os, sys, random, time, glob
 
+import cv2
+from scipy.spatial import ConvexHull
 import numpy as np
 import tensorflow as tf
 from PIL import Image
@@ -65,8 +67,8 @@ def gaussian2D(shape, sigma_x=1, sigma_y=1):
 
 def draw_truncate_gaussian(heatmap, center, h_radius, w_radius, k=1):
     h, w = 2 * h_radius + 1, 2 * w_radius + 1
-    sigma_x = w / 6
-    sigma_y = h / 6
+    sigma_x = w / 6.0
+    sigma_y = h / 6.0
     gaussian = gaussian2D((h, w), sigma_x=sigma_x, sigma_y=sigma_y)
 
     y, x = int(center[0]), int(center[1])
@@ -117,12 +119,18 @@ def gen_ord_mask(mask, n=16):
         if len(np.where(mask == i+1)[0]) == 0:
             continue
 
-        m = np.where(mask == i+1, 1, 0)
+        m = np.squeeze(np.where(mask == i+1, 1, 0), axis=-1)
+
+        # m = np.argwhere(m)
+        # hull = ConvexHull(m)
+        # points = np.array(m[hull.vertices], dtype=np.int32)[:, ::-1]
+        # img_slice = arr[:, :, i].copy()
+        # cv2.fillPoly(img_slice, pts=[points], color=(1,))
+        # arr[:, :, i] = img_slice
+    
         rmin, rmax = np.where(np.any(m, axis=1))[0][[0, -1]]
         cmin, cmax = np.where(np.any(m, axis=0))[0][[0, -1]]
-
         arr[rmin:rmax+1, cmin:cmax+1, i] = 1
-        # arr[:, :, i] = m
 
     return arr
 
@@ -148,19 +156,19 @@ def preprocess_image(mask_path, image_size=256):
 
     char_mask = np.where(mask > 0.5, 1, 0)
 
-    # bbox = get_bbox(mask_path, target_size=(image_size, image_size))
-    # heat_map = draw_heatmaps((1, image_size, image_size, 1), [bbox])
-    # heat_map = np.squeeze(heat_map, axis=0)
+    bbox = get_bbox(mask_path, target_size=(image_size, image_size))
+    heat_map = draw_heatmaps((1, image_size, image_size, 1), [bbox])
+    heat_map = np.squeeze(heat_map, axis=0)
 
     ord_mask = gen_ord_mask(mask)
-    # char_ord = one_hot(mask, 16)
+    # ord_mask = one_hot(mask, 16)
 
     # # show ord mask
-    # plt.imshow(np.max(char_ord[:, :, :5], axis=-1))
+    # plt.imshow(np.max(ord_mask[:, :, 4:], axis=-1))
     # plt.show()
     # quit()
 
-    return np.concatenate([char_mask, ord_mask], axis=-1, dtype=np.float32)
+    return np.concatenate([heat_map, char_mask, ord_mask], axis=-1, dtype=np.float32)
 
 
 if __name__ == "__main__":
@@ -169,6 +177,8 @@ if __name__ == "__main__":
     IMAGE_SIZE = 256
 
     mask_path = glob.glob(PATH + '/mask/*.png')
+    # shuffle
+    random.shuffle(mask_path)
 
     for f in mask_path:
         d = preprocess_image(f)